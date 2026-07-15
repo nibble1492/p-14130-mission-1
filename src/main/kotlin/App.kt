5933e51c67661776d2263644445495a4f6f9c9a2
JUNHYEONG LEE <nibble1492@gmail.com>
2026-07-15 12:16:09 +0900

    4&5단계 완료

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,7 +2,7 @@ package com
 
 class App {
     private var lastId = 0;
-
+    val wiseSayings = mutableListOf<WiseSaying>()
     fun run() {
         println("== 명언 앱 ==")
 
@@ -21,7 +21,16 @@ class App {
                     val author = readln().trim()
 
                     lastId++
+
+                    wiseSayings.add(WiseSaying(lastId, content, author))
                     println("${lastId}번 명언이 등록되었습니다.")
+                }
+                "목록" -> {
+                    println("번호 / 작가 / 명언")
+                    println("---------------------")
+                    for (wiseSaying in wiseSayings.reversed()) {
+                        println("${wiseSaying.id} / ${wiseSaying.author} / ${wiseSaying.content}")
+                    }
                 }
             }
         }
--- src/main/kotlin/WiseSaying.kt
+++ src/main/kotlin/WiseSaying.kt
@@ -1,2 +1,3 @@
 package com
 
+data class WiseSaying(val id: Int, val content: String, val author: String)
