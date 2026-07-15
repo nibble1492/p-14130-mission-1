968a726d4730438af1763e5f63388c5c03e0d61e
JUNHYEONG LEE <nibble1492@gmail.com>
2026-07-15 12:32:46 +0900

    6단계 완료

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -9,8 +9,10 @@ class App {
         while (true) {
             print("명령) ")
             val command = readln().trim()
+            val cmdBits = command.split("?", limit = 2)
+            val actionName = cmdBits[0]
 
-            when (command) {
+            when (actionName) {
                 "종료" -> {
                     break
                 }
@@ -32,7 +34,26 @@ class App {
                         println("${wiseSaying.id} / ${wiseSaying.author} / ${wiseSaying.content}")
                     }
                 }
+                "삭제" -> {
+                    val id = parseId(cmdBits)
+                    if (id != null) {
+                        wiseSayings.removeIf { it.id == id }
+                        println("${id}번 명언이 삭제되었습니다.")
+                    }
+                }
             }
+        }
+    }
+
+    private fun parseId(cmdBits: List<String>): Int? {
+        if(cmdBits.size < 2) return null
+
+        val queryBit = cmdBits[1].split("=", limit = 2)
+        return if (queryBit[0] == "id" && queryBit.size > 1) {
+            queryBit[1].toIntOrNull()
+        }
+        else {
+            null
         }
     }
 }
